46dcd04b37bebd5927f03b4110eff3b6e412ec62
lpeng <3443002624@qq.com>
2022-03-31 10:04:13 +0800

    📝 test: add test component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <test-component />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import TestComponent from './components/TestComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    TestComponent
   }
 }
 </script>
---
+++ src/components/TestComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>devil</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestComponent',
+  components: {},
+  data() {
+    return {}
+  },
+//   props: [''],
+//   inject: [''],
+  provide() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+  beforeCreate() {},
+  created() {
+    const t = { a: 2e3, b: 3e3 }
+    console.log('t', t?.name?.ss ?? 'is', t)
+  },
+  beforeMount() {},
+  mounted() {},
+  beforeUpdated() {},
+  updated() {},
+  beforeDestroy() {},
+  destroyed() {}
+}
+</script>
+<style lang='' scoped>
+</style>
